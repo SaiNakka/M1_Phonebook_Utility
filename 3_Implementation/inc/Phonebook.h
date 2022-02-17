7dab205faefd2e8fd9be831d301b984995b61a41
Nakka Sai Venkatesh <57895309+SaiNakka@users.noreply.github.com>
2022-02-18 03:08:08 +0530

    Create Phonebook.h

---
+++ 3_Implementation/inc/Phonebook.h
@@ -0,0 +1,35 @@
+/**
+ * @file Phonebook.h
+ * @author Sai Nakka
+ * @brief Header file for function prototypes
+ * @version 0.1
+ * @date 2021-11-21
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ * 
+ */
+#ifndef __PHONEBOOK_H__
+#define __PHONEBOOK_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+    char name[35];
+
+    char address[50];
+
+    char father_name[35];
+
+    char mother_name[30];
+
+    long int mble_no;
+
+    char sex[8];
+
+    char mail[100];
+
+    char citision_no[20];
+   
+
+#endif  /* #define __PHONEBOOK_H__ */
